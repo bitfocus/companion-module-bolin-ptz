639ffc3b30bbdf298949a507b242140d76e81a06
bryce <50299051+bryce-seifert@users.noreply.github.com>
2025-11-16 17:58:40 -0800

    wip: feedback creation funcitons

--- src/feedbacks.ts
+++ src/feedbacks.ts
@@ -5,13 +5,109 @@ import { CompanionFeedbackDefinitions } from '@companion-module/base'
 export function UpdateFeedbacks(self: ModuleInstance): void {
 	const feedbacks: CompanionFeedbackDefinitions = {}
 
+	function createToggleFeedback(feedbackID: string, name: string, description: string, callback: () => boolean): void {
+		feedbacks[feedbackID] = {
+			name: name,
+			description: description,
+			type: 'boolean',
+			defaultStyle: {
+				bgcolor: 0x009900,
+			},
+			options: [],
+			callback: callback,
+		}
+	}
+
+	function createValueFeedback(
+		feedbackID: string,
+		name: string,
+		description: string,
+		defaultValue: number,
+		currentValue: number,
+	): void {
+		feedbacks[feedbackID] = {
+			name: name,
+			description: description,
+			type: 'boolean',
+			defaultStyle: {
+				bgcolor: 0x009900,
+			},
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Comparison',
+					id: 'comparison',
+					choices: [
+						{ label: 'Equal to', id: 'equal' },
+						{ label: 'Greater than', id: 'greaterThan' },
+						{ label: 'Less than', id: 'lessThan' },
+					],
+					default: 'equal',
+				},
+				{
+					type: 'textinput',
+					label: 'Value',
+					id: 'value',
+					default: defaultValue.toString(),
+				},
+			],
+			callback: (feedback: any) => {
+				const comparison = feedback.options.comparison as 'equal' | 'greaterThan' | 'lessThan'
+				const value = feedback.options.value as number
+
+				switch (comparison) {
+					case 'equal':
+						return currentValue === value
+					case 'greaterThan':
+						return currentValue > value
+					case 'lessThan':
+						return currentValue < value
+				}
+			},
+		}
+	}
+
+	createValueFeedback('gain', 'Gain', 'Gain', 50, self.camera?.currentExposureInfo()?.Gain ?? 0)
+
+	createToggleFeedback('flip', 'Flip', 'Flip enabled', () => {
+		return self.camera?.currentPictureInfo()?.Flip ?? false
+	})
+
+	createToggleFeedback('mirror', 'Mirror', 'Mirror enabled', () => {
+		return self.camera?.currentPictureInfo()?.Mirror ?? false
+	})
+
+	createToggleFeedback('hlcMode', 'HLC Mode', 'HLC mode enabled', () => {
+		return self.camera?.currentPictureInfo()?.HLCMode ?? false
+	})
+
+	createToggleFeedback('blcMode', 'BLC Mode', 'BLC mode enabled', () => {
+		return self.camera?.currentPictureInfo()?.BLC ?? false
+	})
+
+	createToggleFeedback('smart', 'Smart Focus', 'Smart focus enabled', () => {
+		return self.camera?.currentLensInfo()?.SmartFocus ?? false
+	})
+
+	createToggleFeedback('digitalZoom', 'Digital Zoom', 'Digital zoom enabled', () => {
+		return self.camera?.currentLensInfo()?.DigitalZoom ?? false
+	})
+
+	createToggleFeedback('zoomRatioOSD', 'Zoom Ratio OSD', 'Zoom ratio OSD enabled', () => {
+		return self.camera?.currentLensInfo()?.ZoomRatioOSD ?? false
+	})
+
+	createToggleFeedback('wdr', 'WDR', 'WDR enabled', () => {
+		return self.camera?.currentGammaInfo()?.WDR ?? false
+	})
+
+	createToggleFeedback('smartExposure', 'Smart Exposure', 'Smart exposure enabled', () => {
+		return self.camera?.currentExposureInfo()?.SmartExposure ?? false
+	})
+
 	feedbacks['positionLimitEnabled'] = {
 		name: 'Position Limit Enabled',
 		description: 'Locked position',
-		callback: (feedback: any) => {
-			const direction = feedback.options.direction as keyof PositionLimitations
-			return self.camera?.getState().positionLimitations?.[direction] ?? false
-		},
 		type: 'boolean',
 		defaultStyle: {
 			bgcolor: 0x009900,
@@ -30,6 +126,10 @@ export function UpdateFeedbacks(self: ModuleInstance): void {
 				id: 'direction',
 			},
 		],
+		callback: (feedback: any) => {
+			const direction = feedback.options.direction as keyof PositionLimitations
+			return self.camera?.getState().positionLimitations?.[direction] ?? false
+		},
 	}
 	self.setFeedbackDefinitions(feedbacks)
 }
